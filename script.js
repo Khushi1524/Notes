6a82ec3f3234495d8a06967fb8879c12bede0d7d
Khushi1524 <khushitiwari2415@gmail.com>
2024-12-17 18:01:56 +0530

    added the signup page

--- script.js
+++ script.js
@@ -1,43 +1,69 @@
-const addnote = document.querySelector('#newnote');
-const noteContainer = document.querySelector('#noteContainer');
+const addnote = document.querySelector("#newnote");
+const noteContainer = document.querySelector("#noteContainer");
 
-function showNotes(){
-    noteContainer.innerHTML = localStorage.getItem("notes")
+function showNotes() {
+  noteContainer.innerHTML = localStorage.getItem("notes");
 }
 
 // showNotes();
 
-function updateStorage(){
-    // console.log("updated");
-    localStorage.setItem("notes",noteContainer.innerHTML)
+function updateStorage() {
+  // console.log("updated");
+  localStorage.setItem("notes", noteContainer.innerHTML);
 }
 
 addnote.addEventListener("click", (event) => {
-    const newCard = document.createElement("div");
-    newCard.classList.add("card");
+  const newCard = document.createElement("div");
+  newCard.classList.add("card");
 
-    newCard.innerHTML = `
+  newCard.innerHTML = `
         <textarea name="" id=""></textarea>
         <i id="delete" class="fa-solid fa-trash"></i>
         <i id="edit" class="fa-solid fa-pen-to-square"></i>
     `;
 
-    noteContainer.appendChild(newCard);
-    updateStorage()
+  noteContainer.appendChild(newCard);
+  updateStorage();
 
-    newCard.querySelector("#delete").addEventListener("click", () => {
-        newCard.remove();
-        updateStorage()
-    });
+  newCard.querySelector("#delete").addEventListener("click", () => {
+    newCard.remove();
+    updateStorage();
+  });
 
-    newCard.querySelector('textarea').addEventListener("input",()=>{
-        updateStorage();
-    })
+  newCard.querySelector("textarea").addEventListener("input", () => {
+    updateStorage();
+  });
 });
-
 
 // const mode = document.querySelector('#mode')
 // const body = document.querySelector('body')
 // mode.addEventListener('click',(event)=>{
 //     body.style.backgroundColor = "black";
 // })
+
+//signup
+
+const signUp = document.querySelector("#signup");
+const signupContainer = document.querySelector(".signupContainer");
+const formClose = document.querySelector("#formClose");
+
+signUp.addEventListener("click", (e) => {
+  signupContainer.style.display = "flex";
+});
+
+formClose.addEventListener("click", (e) => {
+  signupContainer.style.display = "none";
+});
+
+// const login = document.querySelector("#login");
+// const inputName = document.querySelector("#inputName");
+// const formHeading = document.querySelector("#formHeading");
+// const submit = document.querySelector("#submit");
+// const textBelow = document.querySelector("#textBelow");
+// login.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   inputName.style.display = "none";
+//   formHeading.innerHTML = "Login";
+//   submit.innerHTML = "Login";
+//   textBelow.style.display = 'none'
+// });
